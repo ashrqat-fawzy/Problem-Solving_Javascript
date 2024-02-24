2b13e6fe3512ed7c35211be18f81f248cb9bfb09
Ashrqat <91925694+ashrqat-fawzy@users.noreply.github.com>
2024-02-24 20:50:56 +0200

    Create prob-4.js

---
+++ prob-4.js
@@ -0,0 +1,18 @@
+//https://vjudge.net/problem/AtCoder-abc068_a
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question("", input => {
+  // const [a, b] =  input.split(" ");
+  
+  let A= parseInt(input);
+  // let B= parseInt(b);
+  
+console.log(`ABC${A}`);
+  
+
+  readline.close();
+});
